18d8ea3bf4411eaaf0cf991e44f4af14708fab94
Alexandr <jint0ro7292@gmail.com>
2022-04-30 16:56:15 +0500

    test 1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+let access = parseInt("11000", 2);
+
+alert( access );
+
+let access2 = access.toString(2);
+alert( access2 );
